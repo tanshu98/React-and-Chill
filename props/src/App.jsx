4608aaf9d0e02085c1220a8cfb8d1fdf246d76de
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-02-08 15:39:10 +0530

    Added React and Props file

---
+++ props/src/App.jsx
@@ -0,0 +1,15 @@
+import MediaCard from '../Components/Card/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='heading'>
+    <span className='heading-info'>React And Props</span>
+        <MediaCard />
+      </div>
+    </div>
+  );
+}
+
+export default App;
