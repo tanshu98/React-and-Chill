4d2e53c5213f094c899a0549d096ef4daea99f2b
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-02-09 10:26:55 +0530

    Added Props FIles - Learnt about Props and their usecases

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,12 +1,24 @@
-import MediaCard from '../Components/Card/Card';
-import './App.css';
+import "./App.css";
+import Card from "./Components/Card/Card";
 
 function App() {
+  // let myObj = {
+  //   id: 1, 
+  //   username: 'tanshu'
+  // }
+  // Similarly i can pass array also..
+  let myArr = [1,2,3];
   return (
-    <div className='App'>
-      <div className='heading'>
-    <span className='heading-info'>React And Props</span>
-        <MediaCard />
+    <div className="App">
+      <div className="heading">
+        <span className="heading-info">React And Props</span>
+        <div className="card">
+        {/* <Card data = "Vistaa" myObj= {myObj.username} /> */}
+        {/* <Card data = "Vistaa" myObj= {myArr} /> */}
+        <Card name= 'Tanshu'/>
+        <Card name = 'Allewar'/>
+
+        </div>
       </div>
     </div>
   );
